c4d92041b533bfe1e0abf6d5d84868fb10aef948
Bruno Pimentel <bpimentel@mastertech.tech>
2018-08-28 08:28:38 -0300

    Grava uma nova tarefa

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
+
 const comando = process.argv[2];
+const entrada = process.argv[3];
+const listaTarefas = [];
 
 if(comando === 'inserir'){
-    console.log('O comando foi inserir');
+    let tarefa = {
+        conteudo: entrada,
+        feita: false
+    }
+
+    listaTarefas.push(tarefa);
+
+    let listaTarefasString = JSON.stringify(listaTarefas);
+
+    fs.writeFile('tarefas.json', listaTarefasString, function(erro){
+        console.log(erro);
+    });
 } else if(comando === 'listar'){
     console.log('O comando foi listar');
 } else if(comando === 'fazer'){
