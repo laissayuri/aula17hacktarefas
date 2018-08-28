1d70971767ee5ca103367d88b41e3df7832eec35
laissayuri <k.lais.sayuri@gmail.com>
2018-08-28 09:48:04 -0300

    Lista de tarefas com todos os comandos

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
 const fs = require('fs');
 
-const comando = process.argv[2];
-const entrada = process.argv[3];
-const listaTarefas = [];
+let comando = process.argv[2];
+let entrada = process.argv[3];
+let listaTarefas = [];
 
-if(comando === 'inserir'){
-    let tarefa = {
-        conteudo: entrada,
-        feita: false
+
+fs.readFile("tarefas.json", {encoding: "UTF-8"}, function(erro,dados){
+    if(!erro){
+        listaTarefas = JSON.parse(dados);
+    }
+    
+    if(comando === 'inserir'){
+        let tarefa = {
+            conteudo: entrada,
+            feita: false
     }
 
     listaTarefas.push(tarefa);
+
+    console.log("Tarefa inserida com sucesso");
 
     let listaTarefasString = JSON.stringify(listaTarefas);
 
     fs.writeFile('tarefas.json', listaTarefasString, function(erro){
         console.log(erro);
     });
-} else if(comando === 'listar'){
-    console.log('O comando foi listar');
-} else if(comando === 'fazer'){
-    console.log('O comando foi fazer');
-} else if(comando === 'limpar'){
-    console.log('O comando foi limpar');
-} else {
-    console.log('Comando inválido.');
-}
+
+
+    } else if(comando === 'listar'){
+
+        for(let i = 0; i <listaTarefas.length; i++){
+            let tarefa = listaTarefas[i];
+            let status = tarefa.feita ? "feita" : "pendente";
+            //é um if: confere a tarefa.feita. Se for true, dá feita. Se for false, dá pendente;
+            listaTarefas[i] = `${i+1} - ${tarefa.conteudo} - ${status}`;
+        }
+        let listaTarefasString = JSON.stringify(listaTarefas);
+        console.log(listaTarefasString);
+        console.log('O comando foi listar');
+
+
+    } else if(comando === 'fazer'){
+        
+        let tarefa;
+        for(let i = 0; i<listaTarefas.length; i++){
+            tarefa = listaTarefas[i];
+            if (tarefa.conteudo === entrada){
+                tarefa.feita = true;
+            }
+        }
+        console.log('O comando foi fazer');
+
+        let listaTarefasString = JSON.stringify(listaTarefas);
+        fs.writeFile('tarefas.json', listaTarefasString, function(erro){
+            console.log(erro);
+        });
+
+
+    } else if(comando === 'limpar'){
+        listaTarefas = [];
+        console.log('O comando foi limpar');
+
+        let listaTarefasString = JSON.stringify(listaTarefas);
+        fs.writeFile('tarefas.json', listaTarefasString, function(erro){
+            console.log(erro);
+        });
+        
+    } else {
+        console.log('Comando inválido.');
+    }
+
+
+
+});
